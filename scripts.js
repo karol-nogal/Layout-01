469046063642dad72f837e15f41946ea8b048583
karol-nogal <karolnogal92@gmail.com>
2020-05-26 01:23:46 +0200

    slider buttons works on text also

--- scripts.js
+++ scripts.js
@@ -13,3 +13,19 @@ function prevSlide(){
     index = (index + 1 + slides.length) % slides.length
     slides[index].classList.add("active")
 }
+
+const slideshowText = document.getElementById("slideshowText");
+const slidesText = slideshowText.getElementsByTagName('div')
+let i = 0;
+
+function nextSlideText(){
+    slidesText[i].classList.remove("active")
+    i = (i+1)%slidesText.length
+    slidesText[i].classList.add("active")
+}
+
+function prevSlideText(){
+    slidesText[i].classList.remove("active")
+    i = (i + 1 + slidesText.length) % slidesText.length
+    slidesText[i].classList.add("active")
+}
